801ca1ed93047e5586e08e20da9ebc216ec9e64c
Alisher <alisherxujanov163@gmail.com>
2026-07-20 00:40:14 +0500

    feat: initialize demo project with Next.js and basic components
    - Added package.json with Next.js and React dependencies
    - Created SVG assets for the project
    - Implemented Heading component to display a greeting
    - Set up global styles and layout for the application
    - Created main page to render the Heading component

---
+++ demo/src/app/components/Heading.jsx
@@ -0,0 +1,8 @@
+const Heading = () => {
+  return (
+    <h1>Hello Tuychibek</h1>
+  )
+}
+
+
+export default Heading;
---
+++ demo/src/app/page.jsx
@@ -0,0 +1,15 @@
+import Heading from "./components/Heading";
+
+export default function Home() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+}
+
+
+// JSX  =>  JavaScript XML
+
+// export {}
+// export default
