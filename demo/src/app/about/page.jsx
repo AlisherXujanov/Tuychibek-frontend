90baf0824d434c7b7351292437caf3c0a5d07e15
Alisher <alisherxujanov163@gmail.com>
2026-08-14 00:19:41 +0500

    feat: initialize demo project with Next.js and essential configurations
    - Added .gitignore to exclude unnecessary files and directories.
    - Created AGENTS.md and CLAUDE.md for agent rules and references.
    - Configured ESLint with custom settings for Next.js.
    - Set up jsconfig.json for path mapping in the project.
    - Established next.config.mjs for Next.js configuration options.
    - Created package.json and package-lock.json for project dependencies.
    - Added README.md for project documentation and setup instructions.
    - Included SVG assets for visual elements in the public directory.
    - Developed initial layout and pages (Home and About) with basic styling.

---
+++ demo/src/app/about/page.jsx
@@ -0,0 +1,13 @@
+function About() {
+    return (
+        <>
+            <h1>Hello world</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente delectus dolores veritatis incidunt, reprehenderit corporis non in quas facilis. Cumque animi dignissimos voluptas vitae totam autem odit architecto temporibus.</p>
+        </>
+    )
+}
+
+
+export default About
+
+
---
+++ demo/src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente delectus dolores veritatis incidunt, reprehenderit corporis non in quas facilis. Cumque animi dignissimos voluptas vitae totam autem odit architecto temporibus.</p>
+    </div>
+  );
+}
